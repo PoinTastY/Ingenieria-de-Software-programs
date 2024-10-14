163fa24cf738377766827e175f3f04598b47ce44
pointasty <kbecerra774@gmail.com>
2024-10-14 01:41:46 -0600

    ready to scale

--- Farmacia Pancrasia/Interface/db_repo.py
+++ Farmacia Pancrasia/Interface/db_repo.py
@@ -5,16 +5,16 @@ from Domain.Entities.usuario import Usuario
 
 class DbRepo:
     def __init__(self):
-        self.conn = psycopg2.connect(database = "taller_mecanico",
-                                    user = "postgres",
+        self.conn = psycopg2.connect(database = "pdv",
+                                    user = "admin",
                                     host= 'localhost',
-                                    password = "Isee420.69&hear",
+                                    password = "staremedic1",
                                     port = 5432)
         
         self.cursor = self.conn.cursor()
 
     def login(self, user : str, password : str):
-        self.cursor.execute("SELECT * FROM usuarios WHERE nombre = %s AND password = %s", (user, password))
+        self.cursor.execute("SELECT * FROM usuario WHERE nombre = %s AND password = %s", (user, password))
         user = self.cursor.fetchone()
         if user:
             return Usuario(user[0], user[1], user[2], user[3])
